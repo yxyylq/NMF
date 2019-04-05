e08d576d5c704b67bad52aae796524de23744167
yxyylq <30745812+yxyylq@users.noreply.github.com>
2019-04-05 11:41:50 +0100

    Update nmf.R

--- R/nmf.R
+++ R/nmf.R
@@ -1711,8 +1711,8 @@ function(x, rank, method
                     ffwarning <- function(...){ message('WARNING'); fwarning(...) }
                     
                     # check for NA deviance
-                    resids <- sapply(res.runs, '[[', 'deviance')
-                    if( length(rNA <- which(is.na(resids) )) ){
+                    resids <- as.vector(sapply(res.runs, '[[', 'deviance'))
+                    if( length(rNA <- which(is.na(resids) | is.nan(resids)) ){
                         if( length(rNA) <  nrun ) ffwarning("Some of the computed final deviances are NAs or NaNs [", length(rNA), "]")
                         else ffstop("All runs returned NA or NaN final deviances")
                     }
